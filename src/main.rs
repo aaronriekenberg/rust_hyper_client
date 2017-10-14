e8174d15089c617749d7ab4bb7240f8da96cbc49
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2017-10-14 14:20:40 -0500

    Add logging.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,58 @@
+extern crate chrono;
+extern crate fern;
 extern crate futures;
 extern crate hyper;
+#[macro_use] extern crate log;
 extern crate tokio_core;
 
-use std::io::{self, Write};
+use chrono::prelude::Local;
+
 use futures::{Future, Stream};
+
 use hyper::Client;
+
+use std::io;
+use std::thread;
+
 use tokio_core::reactor::Core;
 
+fn initialize_logging() -> Result<(), fern::InitError>{
+  fern::Dispatch::new()
+    .level(log::LogLevelFilter::Info)
+    .format(|out, message, record| {
+      out.finish(
+        format_args!("{} [{}] {} {} - {}",
+          Local::now().format("%Y-%m-%d %H:%M:%S%.3f %z"),
+          thread::current().name().unwrap_or("UNKNOWN"),
+          record.level(),
+          record.target(),
+          message
+        )
+      )
+    })
+    .chain(io::stdout())
+    .apply()?;
+
+  Ok(())
+}
+
 fn main() {
+  initialize_logging().expect("failed to initialize logging");
+
   let mut core = Core::new().expect("error creating core");
+
   let client = Client::new(&core.handle());
 
   let uri = "http://raspberrypi:8081".parse().expect("unvalid uri");
-  let work = client.get(uri).and_then(|res| {
-    println!("Response: {:#?}", res);
 
-    res.body().for_each(|chunk| {
-      io::stdout()
-          .write_all(&chunk)
-          .map_err(From::from)
-    })
-  });
+  info!("uri = {}", uri);
 
-  let result = core.run(work);
-  match result {
-    Ok(_) => println!("\nsuccess"),
-    Err(e) => println!("\nerror: {}", e)
+  let work = client.get(uri).and_then(|res| res.body().concat2());
+
+  info!("call core.run");
+
+  match core.run(work) {
+    Ok(body) => info!("success body_string = {}", String::from_utf8_lossy(&body)),
+    Err(e) => info!("error: {}", e)
   };
 }
